dfce0687aef33d6e485a22e4b34dc94cb1ff768a
Hayden <haydenhornsby@hotmail.com>
2023-01-04 18:03:22 +0000

    Hero is now more responsive and comprehensive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,21 +2,6 @@ import React, { useEffect, useState } from 'react';
 import './Hero.scss';
 
 function Hero() {
-    // const heroes = document.querySelectorAll('[data-hero]');
-    // const heroDividers = document.querySelectorAll('[data-hero-divider]');
-    // const heroTexts = document.querySelectorAll('[data-hero-text]');
-    // const heroArrows = document.querySelectorAll('[data-hero-arrow]');
-
-    // function loadHero() {
-    //     heroDividers.forEach(divider => divider.classList.add('is-active'));
-    //     heroTexts.forEach(text => text.classList.add('is-active'));
-    //     heroArrows.forEach(arrow => arrow.classList.add('is-active'));
-    // };
-
-    // window.addEventListener('DOMContentLoaded', (event) => {
-    //     setTimeout(loadHero, 1000);
-    // });
-
     const [ isActive, setIsActive ] = useState(false);
 
     useEffect(() => {
