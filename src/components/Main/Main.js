023a1d1d4289b1a3ef8f92229b7b7a61924f3806
Hayden <haydenhornsby@hotmail.com>
2023-01-09 12:52:44 +0000

    Website now has a main content section

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 import './Main.scss';
 
+import About from '../About/About';
+import Projects from '../Projects/Projects';
+
 function main() {
     return (
-        <div></div>
+        <div className='c-main c-main__wrapper c-main__wrapper--light'>
+            <div className='c-main__about'>
+                <div class='c-main__anchor-offset' id='about'></div>
+                <About />
+            </div>
+            <div className='c-main__projects'>
+                <div class='c-main__anchor-offset' id='projects'></div>
+                <div class='c-main__border--top'></div>
+                <Projects />
+            </div>
+        </div>
     );
 }
 
