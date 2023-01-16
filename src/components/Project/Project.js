603898f37737c9be1bf93d02843180af6095ad29
Hayden <haydenhornsby@hotmail.com>
2023-01-16 11:22:05 +0000

    Projects now have conditional rendering and error handling in case of missing image

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -3,17 +3,24 @@ import React from 'react';
 import './Project.scss';
 
 function project(props) {
-    const imageLinkMobile = props.imageLinkMobile;
-    const imageLinkDesktop = props.imageLinkDesktop;
+    var imageLinkMobile = props.imageLinkMobile;
+    var imageLinkDesktop = props.imageLinkDesktop;
     const projectLink = props.projectLink;
     const projectGitLink = props.projectGitLink;
     const projectName = props.projectName;
     const projectDescript = props.projectDescript;
 
+    if (imageLinkMobile == "" || imageLinkMobile == null) {
+        imageLinkMobile = imageLinkDesktop;
+    } else if (imageLinkDesktop == "" || imageLinkDesktop == null) {
+        imageLinkDesktop = imageLinkMobile;
+    }
+
     return (
         <div className='c-project c-project__container'>
             <div className='c-project__content'>
-                <div class='c-project__example-pic'>
+                {
+                <div class={`c-project__example-pic ${(imageLinkMobile) ? "" : "c-project__example-pic--error-mobile"} ${(imageLinkDesktop) ? "" : "c-project__example-pic--error-desktop"}`}>
                     <img class='c-project__pic--mobile' src={imageLinkMobile} />
                     <img class='c-project__pic--desktop' src={imageLinkDesktop} />
                     <a class='c-project__img-overlay c-project__img-overlay--top'
@@ -24,7 +31,7 @@ function project(props) {
                         href={projectGitLink} target='_blank'>
                         <h4>View On GitHub!</h4>
                     </a>
-                </div>
+                </div>}
                 <div class='c-project__text'>
                     <h4 class='c-project__name u-text--white'>{projectName}</h4>
                     <p class='c-project__descript'>{projectDescript}</p>
